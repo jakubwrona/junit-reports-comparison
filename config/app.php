d78ace358db5fa17e01babaf1357abaeb3bbbb82
Jakub Wrona <km.wrona@gmail.com>
2017-11-27 16:56:13 +0100

    features as tabs, config

---
+++ config/app.php
@@ -0,0 +1,15 @@
+<?php
+return [
+    'reports' => [[
+        'label' => 'L4.2 v1',
+        'path' => '/home/kuba/www/malachite-codeception-2/bdd/tests/_output/report-new-single.xml'
+    ], [
+        'label' => 'L5.5 v1',
+        'path' => '/home/kuba/www/malachite-codeception-2/bdd/tests/_output/report-old-single.xml'
+    ]],
+    'primary' => 'L4.2 v1',
+    'query' => '//testcase',
+    'table' => [
+        'pageLength' => 30
+    ]
+];
--- public/index.php
+++ public/index.php
@@ -1,146 +1,224 @@
 <?php
-    $oldV1doc = new DOMDocument();
-    $oldV1doc->preserveWhiteSpace = false;
-    //$oldV1doc->loadXML(trim(file_get_contents('/home/kuba/www/malachite-codeception-2/bdd/tests/_output/report-old-v1.xml')));
-    $oldV1doc->loadXML(trim(file_get_contents('/home/kuba/www/malachite-codeception-2/bdd/tests/_output/report-old-2.xml')));
-
-
-    $newV1doc = new DOMDocument();
-    $newV1doc->preserveWhiteSpace = false;
-    //$newV1doc->loadXML(trim(file_get_contents('/home/kuba/www/malachite-codeception-2/bdd/tests/_output/report-new-v1.xml')));
-    $newV1doc->loadXML(trim(file_get_contents('/home/kuba/www/malachite-codeception-2/bdd/tests/_output/report-new-2.xml')));
-
-    $domXpath = new DOMXPath($oldV1doc);
-    $tests = $domXpath->query('//testcase');
+    $config = include('../config/app.php');
 
     $res = [];
-    $total = [
-            'new' => 0,
-            'old' => 0
-    ];
+    $total = [];
+    $features = [];
 
-    foreach ($tests as $item) {
-        $feature = strrchr($item->getAttribute('name'), '/');
-        $name = strrchr($item->getAttribute('name'), '|');
-        if (isset($res[$feature][$name]['old'])) {
-            $name .= ' example 2';
+    foreach ($config['reports'] as $report) {
+        $total[$report['label']] = 0;
+        $doc = new DOMDocument();
+        $doc->preserveWhiteSpace = false;
+        try {
+            $doc->loadXML(trim(file_get_contents($report['path'])));
+        } catch (Exception $e) {
+            die($e->getMessage());
         }
-        $res[$name]['old'] = $item->getAttribute('time');
-        $total['old']+=$item->getAttribute('time');
-    }
-
-    $domXpath = new DOMXPath($newV1doc);
-    $tests = $domXpath->query('//testcase');
-    foreach ($tests as $item) {
-        $feature = strrchr($item->getAttribute('name'), '/');
-        $name = strrchr($item->getAttribute('name'), '|');
-        if (isset($res[$feature][$name]['new'])) {
-            $name .= ' example 2';
+        $domXpath = new DOMXPath($doc);
+        $tests = $domXpath->query($config['query']);
+        foreach ($tests as $item) {
+            $counter = 2;
+            $feature = substr(strrchr($item->getAttribute('file'), '/'), 1);
+            $name = ltrim(str_replace('&', 'amp;', strrchr($item->getAttribute('name'), '|')), '| ');
+            while (isset($res[$feature][$name][$report['label']])) {
+                $name = strrchr($item->getAttribute('name'), '|') . ' example '.$counter++;
+            }
+            $res[$feature][$name][$report['label']] = $item->getAttribute('time');
+            $total[$report['label']]+=$item->getAttribute('time');
+            $features[$feature][$report['label']]+=$item->getAttribute('time');
         }
-        $res[$name]['new'] = $item->getAttribute('time');
-        $total['new']+=$item->getAttribute('time');
     }
 ?>
 <!doctype html>
 <html lang="en">
   <head>
     <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.16/css/jquery.dataTables.min.css">
-    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js?chof=validate"></script>
     <script type="text/javascript">
-      google.charts.load('current', {'packages':['corechart', 'bar']});
-      google.charts.setOnLoadCallback(drawChart);
-      google.charts.setOnLoadCallback(drawChart2);
+      google.charts.load("current", {"packages":["corechart", "bar"]});
+      <?php
+      $counter = 1;
+      echo "\n".'google.charts.setOnLoadCallback(drawMainChart);';
+      /*foreach ($res as $feature => $tests) {
+          echo "\n".'google.charts.setOnLoadCallback(drawChart_' . $counter . ');';
+          echo "\n".'google.charts.setOnLoadCallback(drawChart2_' . $counter++ . ');';
+      }*/
+      ?>
 
-      function drawChart() {
+      function drawMainChart() {
           var data = google.visualization.arrayToDataTable([
-              ['API', 'Total response time'],
-              ['SaC OLD', <?php echo $total['old'];?>],
-              ['SaC NEW', <?php echo $total['new'];?>]
-          ]);
-
+              ["API", "Total response time"],
+              <?php
+              $subCounter = 1;
+              foreach ($total as $label => $value) {
+                  echo '["' . $label . '", ' . $value . ']';
+                  if ($subCounter < count($total)) {
+                      echo ',';
+                  }
+                  $subCounter++;
+              }
+              ?>]);
           var options = {
-              title: 'Total execution time',
+              title: "Total execution time",
               is3D: true
           };
-
-          var chart = new google.visualization.PieChart(document.getElementById('piechart'));
-
+          var chart = new google.visualization.PieChart(document.getElementById("mainPieChart"));
           chart.draw(data, options);
       }
-
-
-      function drawChart2() {
-        var data = google.visualization.arrayToDataTable([
-          ['Example', 'OLD v1', 'NEW v1'],
-          <?php
-            foreach ($res as $example => $data) {
-                echo '[\''.$example.'\', '.$data['old'].','.$data['new'].']';
-                if ($example !== $name) {
-                    echo ',';
+      <?php
+        $counter = 1;
+        foreach ($res as $feature => $tests) {
+            echo "\n\n".'function drawChart_' . $counter . '() {';
+            echo "\n".'var data = google.visualization.arrayToDataTable([';
+            echo '["API", "Total response time"],';
+            $ifLastCounter = 1;
+            foreach ($features[$feature] as $label => $value) {
+                if ($ifLastCounter < count($config['reports'])) {
+                    echo '["' . $label . '", ' . $value . '],';
+                } else {
+                    echo '["' . $label . '", ' . $value . ']';
                 }
+                $ifLastCounter++;
             }
-            //echo '[\'TOTAL\', '.$total['old'].', '.$total['new'].']';
-            ?>
-        ]);
-        var formatter = new google.visualization.NumberFormat({
-              fractionDigits: 3,
-              suffix: ' s'
-        });
-        formatter.format(data, 1);
-        formatter.format(data, 2);
+            echo ']);';
 
-        var options = {
+            echo "\n".'var options = {
+              title: "Total execution time",
+              is3D: true
+          };';
+
+            echo "\n".'var chart = new google.visualization.PieChart(document.getElementById("featureChart_' . $counter . '")); ';
+            echo "\n".'chart.draw(data, options);';
+            echo "\n".'}';
+            echo "\n\n".'function drawChart2_' . $counter . '() {';
+            echo "\n".'var data = google.visualization.arrayToDataTable([';
+            echo json_encode(array_merge(['Example'], array_column($config['reports'], 'label'))).','."\n";
+            $ifLastCounter = 1;
+            foreach ($tests as $example => $data) {
+                echo json_encode(array_merge([$example], array_values($data)), JSON_NUMERIC_CHECK);
+                if ($ifLastCounter < count($res[$feature])) {
+                    echo ','."\n";
+                }
+                $ifLastCounter++;
+            }
+            echo ']);';
+            /*echo "\n".'var formatter = new google.visualization.NumberFormat({
+              fractionDigits: 3,
+              suffix: " s"
+        });';
+            for ($i = 1; $i <= count($config['reports']); $i++) {
+                echo "\n".'formatter.format(data, ' . $i . ');';
+            }*/
+            echo "\n".'var options = {
           chart: {
-            title: 'Starting and Charging API (SaC) response times comparison',
-            subtitle: 'PHP7 Laravel 4 not optimized VS PHP7 Laravel 5 optimized',
+            title: "' . $feature . ' response times comparison"
           },
-          bars: 'horizontal', // Required for Material Bar Charts.
+          bars: "horizontal", // Required for Material Bar Charts.
           hAxis: {
               gridlines: {
                   count: 30
               }}
-        };
-
-        var chart = new google.charts.Bar(document.getElementById('barchart_material'));
-
-        chart.draw(data, google.charts.Bar.convertOptions(options));
-      }
+        };';
+            echo "\n".'var chart = new google.charts.Bar(document.getElementById("barchartMaterial_' . $counter . '"));';
+            echo "\n".'chart.draw(data, google.charts.Bar.convertOptions(options));';
+            echo "\n".'}';
+            $counter++;
+        }
+        ?>
     </script>
     <script type="text/javascript" src="//code.jquery.com/jquery-1.12.4.js"></script>
+    <script type="text/javascript" src="//code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
     <script type="text/javascript" src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
     <style type="text/css">
         td.right {
             text-align: right;
         }  
     </style>
+    <title>Compare tests results from the performance perspective</title>
   </head>
-  <body>
-    <div id="barchart_material" style="width: 100%; height: 600px;"></div>
-    <div id="piechart" style="width: 1200px; height: 500px;"></div>
-    <div class="content">
-        <table id="example" width="100%" cellspacing="0">
-            <thead>
-            <tr>
-                <th>API</th>
-                <th>OLD (ms)</th>
-                <th>NEW (ms)</th>
-                <th>diff (ms)</th>
-                <th>diff %</th>
-            </tr>
-            </thead>
-            <tbody>
+  <div>
+    <div id="tabs">
+        <ul>
+            <li><a href="#tabs-0">All features</a></li>
             <?php
-            foreach ($res as $example => $data) {
-                echo "\n".'<tr><td>'.$example.'</td><td class="right">'.sprintf('%1.2f', $data['old'] * 1000).'</td><td class="right">'.sprintf('%1.2f', $data['new'] * 1000).'</td>';
-                echo '<td class="right">'.sprintf('%1.2f', ($data['old'] - $data['new']) * 1000).'</td><td class="right">'.sprintf('%1.2f', ((($data['old'] - $data['new']) / $data['old']) * 100)).'%</td></tr>';
-            }
-            ?>
-            </tbody>
-        </table>
+            $counter = 1;
+            foreach ($res as $feature => $data) {
+                echo '<li><a href="#tabs-'.$counter++.'">'.$feature.'</a></li>';
+            } ?>
+        </ul>
+        <div id="tabs-0">
+            <div id="mainPieChart" style="width: 100%; height: 500px;"></div>
+        </div>
+        <?php
+        $counter = 1;
+        foreach ($res as $feature => $data) {
+            echo '<div id="tabs-' . $counter . '">';
+            echo '<div id="featureChart_' . $counter . '" style="width: 1200px; height: 500px;"></div><br>';
+            echo '<div id="barchartMaterial_' . $counter . '" style="width: 1200px; height: 600px;"></div><br>';
+            echo '<div class="content">';
+                echo '<table id="example_' . $counter . '" width="100%" cellspacing="0">'; ?>
+                    <thead>
+                    <tr>
+                        <th>API</th>
+                        <?php foreach ($config['reports'] as $report) {
+                            echo '<th>' . $report['label'] . '</th>';
+
+                        }
+                        foreach ($config['reports'] as $report) {
+                            if ($report['label'] != $config['primary']) {
+                                echo '<th>save vs ' . $config['primary'] . ' (ms)</th>';
+                                echo '<th>save % vs ' . $config['primary'] . '</th>';
+                            }
+                        } ?></tr>
+                    </thead>
+                    <tbody>
+                    <?php
+                    foreach ($data as $example => $results) {
+                        echo "\n" . '<tr><td>' . $example . '</td>';
+                        foreach ($results as $label => $value) {
+                            echo '<td class="right">' . sprintf('%1.2f', $value * 1000) . '</td>';
+                        }
+                        foreach ($results as $label => $value) {
+                            if ($label !== $config['primary']) {
+                                echo '<td class="right">' . sprintf('%1.2f', ($results[$config['primary']] - $value) * 1000)
+                                    . '</td>';
+                                echo '<td class="right">' . sprintf('%1.2f',
+                                        ((($results[$config['primary']] - $value) / $results[$config['primary']]) * 100))
+                                    . '%</td>';
+                            }
+                        }
+                        echo '</tr>';
+                    }
+                    ?>
+                    </tbody>
+                    </table>
+            </div>
     </div>
+  <?php
+  $counter++;
+  }
+  ?>
     <script type="text/javascript">
         $(document).ready(function() {
-            $('#example').DataTable();
+            <?php
+            $counter = 1;
+            foreach ($res as $feature) {
+                echo '$("#example_'.$counter++.'").DataTable({"pageLength": '.$config['table']['pageLength'].'});';
+            } ?>
+
+            $("#tabs").tabs({
+                activate: function (event, ui) {
+
+                }
+            });
+
+            $("#tabs").on('tabsactivate', function (event, ui) {
+                var active = $('#tabs').tabs('option', 'active');
+                var counter = $("#tabs ul>li a").eq(active).attr("href").substring(6);
+                window["drawChart_"+counter]();
+                window["drawChart2_"+counter]();
+            });
         });
     </script>
   </body>
